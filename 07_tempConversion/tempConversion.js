b96b0a7ce50e738ad6769c432002673bdb4941b4
Derrick Nyasio <dnyasio2014@gmail.com>
2023-01-06 12:53:41 +0300

    Done up to 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let celcius = (value - 32) * .5556;
+  return Math.round(celcius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let far = (degree * 1.8) + 32;
+  return Math.round(far * 10) / 10
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
