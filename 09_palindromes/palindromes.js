0b7707e298ae654020d9405c869a2f606e6ed6fc
Derrick Nyasio <dnyasio2014@gmail.com>
2023-01-31 16:42:29 +0300

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = str.toLowerCase().replace(/[^A-Z0-9]/g, "");
 
+  //Create a reversed string
+    let reversedStr = newStr.split("").reverse().join("")
+    return newStr === reversedStr ? true : false
 };
 
 // Do not edit below this line
